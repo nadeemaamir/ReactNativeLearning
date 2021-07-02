73c363ebe46775e2ee8607c53e28890af7941a1c
nadeem <nadeem.aamir337@gmail.com>
2021-07-02 22:04:51 +0300

    Init commit
    Init commit

---
+++ Day1/src/App.js
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {Tab, TabView} from 'react-native-elements';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  Header,
+  LearnMoreLinks,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const [index, setIndex] = React.useState(0);
+  return (
+    <>
+      <Tab value={index} onChange={setIndex}>
+        <Tab.Item title="recent" />
+        <Tab.Item title="favorite" />
+        <Tab.Item title="cart" />
+      </Tab>
+
+      <TabView value={index} onChange={setIndex}>
+        <TabView.Item style={{backgroundColor: 'red', width: '100%'}}>
+          <Text h1>Recent</Text>
+        </TabView.Item>
+        <TabView.Item style={{backgroundColor: 'blue', width: '100%'}}>
+          <Text h1>Favorite</Text>
+        </TabView.Item>
+        <TabView.Item style={{backgroundColor: 'green', width: '100%'}}>
+          <Text h1>Cart</Text>
+        </TabView.Item>
+      </TabView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
