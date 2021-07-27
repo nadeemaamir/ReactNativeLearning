440bbcc79406bdaae38591736fdca49735c69446
nadeem <nadeem.aamir337@gmail.com>
2021-07-27 20:52:11 +0300

    Update index.js

--- Day1/src/containers/Login/index.js
+++ Day1/src/containers/Login/index.js
@@ -7,19 +7,25 @@ import Header from '../../components/Header';
 import styles from './styles';
 
 const Login = ({params, UserName}) => {
+  const API_URL = 'https://api.themoviedb.org/3/movie/popular';
+  const API_KEY = 'bb925e230868e5ea561be5d9be231edb';
+  const PARAMS = 'page=1';
+  const BASE_URL = `${API_URL}?api_key=${API_KEY}&${PARAMS}`;
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
-  const url = `http://51.38.40.13:4041/api/Login/UserAuthentication?Username=${username}&Password=${password}`;
+  const baseurl = `http://51.38.40.13:4041/api/Login/UserAuthentication?Username=${username}&Password=${password}`;
+  const todourl = 'https://jsonplaceholder.typicode.com/todos/1';
   const onLogin = async () => {
     try {
       setLoading(true);
-      console.log('url kia bana', url);
-      const response = await fetch(url);
+      console.log('url kia bana', todourl);
+      const response = await axios.get(todourl);
       alert('Login Successfully');
     } catch (error) {
       // eslint-disable-next-line no-alert
-      console.log(error.response.data)
+      console.log(error.response.data);
     } finally {
       setLoading(false);
     }
