3b08c7c7df37b944ca1b646fadd9081a73508e4b
Madeeha Hasan <33419910+madeeha96@users.noreply.github.com>
2021-07-28 23:22:22 +0500

    splash fixes

--- Day1/android/app/src/main/java/com/day1/MainActivity.java
+++ Day1/android/app/src/main/java/com/day1/MainActivity.java
@@ -1,16 +1,33 @@
 package com.day1;
 
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
-public class MainActivity extends ReactActivity {
+import org.devio.rn.splashscreen.SplashScreen;
 
-  /**
+
+
+
+   public class MainActivity extends ReactActivity {
+
+      /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
    // Add this method.
-  
-  @Override
-  protected String getMainComponentName() {
-    return "Day1";
-  }
-}
+    @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         SplashScreen.show(this);  // here
+         super.onCreate(savedInstanceState);
+     }
+     
+   
+     @Override
+     protected String getMainComponentName() {
+       return "Day1";
+     }
+    }
+
+
+
+   
+ 
